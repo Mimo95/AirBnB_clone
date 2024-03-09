6b8e4d9caf4786aaffe9454adf31ade9efab2b2e
mohamed hajjoubi <mh041949@gmail.com>
2024-03-09 08:14:43 -0800

    Update console

--- console.py
+++ console.py
@@ -74,16 +74,25 @@ class HBNBCommand(cmd.Cmd):
             print('(hbnb) ', end='')
         return stop
 
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
+    def do_quit(self, command):
+        """ Method to exit the HBNB console"""
+        exit()
 
-    def do_EOF(self, line):
-        """Exit the program"""
+    def help_quit(self):
+        """ Prints the help documentation for quit  """
+        print("Exits the program with formatting\n")
+
+    def do_EOF(self, arg):
+        """ Handles EOF to exit program """
         print()
-        return True
+        exit()
+
+    def help_EOF(self):
+        """ Prints the help documentation for EOF """
+        print("Exits the program without formatting\n")
 
     def emptyline(self):
+        """ Overrides the emptyline method of CMD """
         pass
 
 
