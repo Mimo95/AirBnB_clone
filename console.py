c0ce94643e531e0ebbb19c7a5a626147da8a91a4
mohamed hajjoubi <mh041949@gmail.com>
2024-03-09 08:27:11 -0800

    update

--- console.py
+++ console.py
@@ -14,8 +14,6 @@ from models.amenity import Amenity
 from models import storage
 import re
 
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -312,3 +310,6 @@ class HBNBCommand(cmd.Cmd):
                 print(count)
             else:
                 print("** class doesn't exist **")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
